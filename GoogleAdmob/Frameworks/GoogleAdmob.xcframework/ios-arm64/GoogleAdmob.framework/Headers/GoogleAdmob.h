0c6f380642f49992efd2ba96181e4ff3ae52fc44
awilliams88 <arpitwilliams88@gmail.com>
2024-08-05 18:55:16 +0530

    Generated GoogleAdmob XCFramework

---
+++ GoogleAdmob/Frameworks/GoogleAdmob.xcframework/ios-arm64/GoogleAdmob.framework/Headers/GoogleAdmob.h
@@ -0,0 +1,11 @@
+//
+//  GoogleAdmob.h
+//  GoogleAdmob
+//
+//  Created by Arpit Williams on 05/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double GoogleAdmobVersionNumber;
+FOUNDATION_EXPORT const unsigned char GoogleAdmobVersionString[];
